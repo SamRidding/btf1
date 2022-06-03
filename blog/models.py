831bfe50f06ca6733680ec170bb63ecdb9ff014b
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-03 11:53:06 +0000

    Add comment model and migrate

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,17 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["posted_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.user}"
