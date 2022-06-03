fedab39f92807d443e5f76f3d212f9196e59d81d
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-03 16:09:03 +0000

    Amend blog post view for posting comments

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class BlogPost(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = Comment.objects.filter(post=post).order_by('-posted_on') 
+        comments = post.comments.order_by("-posted_on")
 
         return render(
             request,
@@ -26,3 +26,28 @@ class BlogPost(View):
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("-posted_on")
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.user = request.user
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "blog/blog_post.html",
+            {
+                "post": post,
+                "comments": comments,
+                "comment_form": comment_form,
+            },
+        ) 
