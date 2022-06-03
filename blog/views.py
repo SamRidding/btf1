dd7ce9be948ccfa05317e3313e079587653d4afc
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-03 12:49:08 +0000

    Create view for comments on blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -14,11 +15,14 @@ class BlogPost(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+        comments = Comment.objects.filter(post=post).order_by('-posted_on') 
 
         return render(
             request,
             "blog/blog_post.html",
             {
                 "post": post,
+                "comments": comments,
+                "comment_form": CommentForm()
             },
         )
