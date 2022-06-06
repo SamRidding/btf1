62632b708e9444f065377eb130e02df68cc2ace1
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-06 14:13:36 +0000

    Update url paths for userprofile

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", views.UserProfile.as_view(), name="userprofile"),
+    # path("", views.UserProfile.as_view(), name="userprofile"),
+    path('fav/<int:id>/', views.add_release, name='add_release'),
+    path("", views.saved_releases, name='userprofile'),
 ]
