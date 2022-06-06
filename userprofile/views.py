1eb5f50f8d1741397e1e3e6eb69a31a94db35ac5
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-06 14:12:28 +0000

    Add functions for save release views in userprofile app

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.views.generic import TemplateView
+from blog.models import Post
 
 
-class UserProfile(TemplateView):
+@ login_required
+def saved_releases(request):
+    new = Post.objects.filter(save_release=request.user)
+    return render(request,
+                  'userprofile/userprofile.html',
+                  {'new': new})
 
-    template_name = "userprofile/userprofile.html"
+
+@ login_required
+def add_release(request, id):
+    post = get_object_or_404(Post, id=id)
+    if post.save_release.filter(id=request.user.id).exists():
+        post.save_release.remove(request.user)
+    else:
+        post.save_release.add(request.user)
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+# class UserProfile(TemplateView):
+
+#     template_name = "userprofile/userprofile.html"
