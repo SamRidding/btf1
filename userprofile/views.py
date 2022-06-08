f031c303d87fd1ca9331e5637c4479f463f5bc10
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-08 21:01:30 +0000

    Add messages for saving releases to userprofile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
 from blog.models import Post
@@ -18,8 +19,10 @@ def add_release(request, id):
     post = get_object_or_404(Post, id=id)
     if post.save_release.filter(id=request.user.id).exists():
         post.save_release.remove(request.user)
+        messages.success(request, 'The release has been removed from your list.')
     else:
         post.save_release.add(request.user)
+        messages.success(request, 'The release has been added to your list.')
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
