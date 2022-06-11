8a9f6498582d2e2a3dbeb674a0d75d2a6a19047c
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-11 13:51:46 +0000

    Add javascript to show/hide search bar

--- static/js/base.js
+++ static/js/base.js
@@ -4,3 +4,16 @@ const navLinks = document.getElementsByClassName('navbar-links')[0];
 burgerMenu.addEventListener('click', e => {
     navLinks.classList.toggle('active')
 });
+
+
+const searchBtn = document.getElementById('search-btn');
+const searchBar = document.getElementById('search');
+
+searchBtn.addEventListener('click', e => {
+    if (searchBar.style.display === 'none') {
+        searchBar.style.display = 'flex';
+    } else {
+        searchBar.style.display = 'none';
+    }
+});
+
