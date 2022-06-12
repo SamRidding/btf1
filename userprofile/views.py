079ec189f920122707a78d8cfdf5fc26db3815b1
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-12 21:15:01 +0000

    Add docstrings to userprofile view functions

--- userprofile/views.py
+++ userprofile/views.py
@@ -7,6 +7,8 @@ from blog.models import Post
 
 @login_required
 def saved_releases(request):
+    """Displays releases saved by the user in their user profile"""
+
     new = Post.objects.filter(save_release=request.user)
     return render(request,
                   'userprofile/userprofile.html',
@@ -15,6 +17,11 @@ def saved_releases(request):
 
 @login_required
 def add_release(request, id):
+    """
+    Allows user to add or remove releases they have saved to their
+    user profile
+    """
+
     post = get_object_or_404(Post, id=id)
     if post.save_release.filter(id=request.user.id).exists():
         post.save_release.remove(request.user)
