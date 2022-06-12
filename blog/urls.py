57e7a3dcdb674711549153bb30e947058268becf
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-12 21:06:48 +0000

    Update blog urls to match new view function names

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("", views.HomePage, name="home"),
+    path("", views.home_page, name="home"),
     path("blog/", views.Blog.as_view(), name="blog"),
-    path("search/", views.SearchBlog, name="search_blog"),
-    path('add_post/', views.AddPost, name='add_post'),
-    path('delete_comment/<int:comment_id>/', views.DeleteComment,
+    path("search/", views.search_blog, name="search_blog"),
+    path('add_post/', views.add_post, name='add_post'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment,
          name="delete_comment"),
-    path('edit_post/<slug:slug>/', views.EditPost, name='edit_post'),
-    path('delete_post/<slug:slug>/', views.DeletePost, name="delete_post"),
+    path('edit_post/<slug:slug>/', views.edit_post, name='edit_post'),
+    path('delete_post/<slug:slug>/', views.delete_post, name="delete_post"),
     path('<slug:slug>/', views.BlogPost.as_view(), name='blog_post'),
 ]
