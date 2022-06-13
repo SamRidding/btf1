786b81b9af44fc642403648f97efb876dd962346
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-13 12:21:10 +0000

    Add mix model and migrate

--- mixes/models.py
+++ mixes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from embed_video.fields import EmbedVideoField
 
-# Create your models here.
+class Mix(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    posted_on = models.DateField(auto_now_add=True)
+    image = models.ImageField()
+    content = models.TextField(blank=True)
+    music_preview = EmbedVideoField(blank=True)
+
+    class Meta:
+        ordering = ["-posted_on"]
+
+    def __str__(self):
+        return str(self.title)
