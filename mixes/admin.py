edb323e8737bc86942e2fbdcd358004800adb133
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-13 13:00:46 +0000

    Register mix model in admin

--- mixes/admin.py
+++ mixes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Mix
 
-# Register your models here.
+
+@admin.register(Mix)
+class MixAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'posted_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
